e0df0afe91a178bf0668acdf1b9058e47c479513
Christopher Lee <csl170000@utdallas.edu>
2021-07-12 10:58:59 -0500

    Better error handling

--- src/lib.rs
+++ src/lib.rs
@@ -41,27 +41,27 @@ pub struct WasmLasZipDecompressor {
 #[wasm_bindgen]
 impl WasmLasZipDecompressor {
     #[wasm_bindgen(constructor)]
-    pub fn new(buf: js_sys::Uint8Array) -> Self {      
+    pub fn new(buf: js_sys::Uint8Array) -> Result<WasmLasZipDecompressor, JsValue> {      
         console_error_panic_hook::set_once();
-          
+
         let mut cursor = std::io::Cursor::new(buf.to_vec());
 
-        let hdr = QuickHeader::read_from(&mut cursor).unwrap_throw();
+        let hdr = QuickHeader::read_from(&mut cursor).expect_throw("Error reading header, is your file in the correct format?");
         
-        cursor.seek(std::io::SeekFrom::Start(hdr.header_size as u64)).unwrap_throw();
-        let laz_vlr = read_vlrs_and_get_laszip_vlr(&mut cursor, &hdr);
+        cursor.seek(std::io::SeekFrom::Start(hdr.header_size as u64)).expect_throw("Error seeking to end of header!");
+        let laz_vlr = read_vlrs_and_get_laszip_vlr(&mut cursor, &hdr).expect_throw("Error getting laszip vlr!");
 
-        cursor.seek(std::io::SeekFrom::Start(hdr.offset_to_points as u64)).unwrap_throw();
-        let decomp = LasZipDecompressor::new(cursor, laz_vlr.expect("Compressed data, but no Laszip Vlr found")).unwrap_throw();
+        cursor.seek(std::io::SeekFrom::Start(hdr.offset_to_points as u64)).expect_throw("Error seeking to beginning of points offset!");
+        let decomp = LasZipDecompressor::new(cursor, laz_vlr).expect_throw("Error in LasZipDecompressor constructor!");
 
-        Self {
+        Ok(Self {
             decompressor: decomp,
             header: WasmQuickHeader::from(hdr)
-        }
+        })
     }
 
     pub fn decompress_many(&mut self, out: &mut [u8]) -> Result<(), JsValue> {
-        self.decompressor.decompress_many(out).unwrap_throw();
+        self.decompressor.decompress_many(out).expect_throw("Error decompressing points! Make sure your ArrayBuffer is the correct size.");
         Ok(())
     }
 }
